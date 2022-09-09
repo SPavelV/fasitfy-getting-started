0474ca128cb564df1bb3112b82c2a3e5e48f6611
Pavel Solovev <p.solovev@articul.ru>
2022-09-09 10:17:49 +0400

    Define blog Routes

---
+++ routes/blogs.js
@@ -0,0 +1,31 @@
+const blogController = require('../controller/blogs');
+
+const routes = [
+  {
+    method: 'GET',
+    url: '/api/blogs',
+    handler: blogController.getAllBlogs,
+  },
+  {
+    method: 'GET',
+    url: '/apl/blogs/:id',
+    handler: blogController.getBlog,
+  },
+  {
+    method: 'POST',
+    url: '/api/blogs',
+    handler: blogController.addBlog,
+  },
+  {
+    method: 'PUT',
+    url: '/api/blogs/:id',
+    handler: blogController.updateBlog,
+  },
+  {
+    method: 'DELETE',
+    url: '/api/blogs/:id',
+    handler: blogController.deleteBlog,
+  },
+];
+
+module.exports = routes;
